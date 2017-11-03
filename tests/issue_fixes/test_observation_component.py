26762327cd76de58b9465872a313bee3b106ea05
hsolbrig <solbrig.harold@mayo.edu>
2017-11-03 13:31:37 -0500

    Add unit test for issue #6 - fails at this point.

--- tests/issue_fixes/test_observation_component.py
+++ tests/issue_fixes/test_observation_component.py
@@ -31,25 +31,31 @@ import unittest
 import os
 from _csv import QUOTE_NONE
 from csv import DictReader
+from typing import Dict
 
 from i2fhirb2.loadfacts import load_facts
-from i2fhirb2.removefacts import remove_facts
 from tests.utils.base_test_case import test_data_directory, test_conf_directory, make_and_clear_directory
 
 
 class ObservationComponentTestCase(unittest.TestCase):
     dirname, _ = os.path.split(os.path.abspath(__file__))
+    output_dir = os.path.abspath(os.path.join(dirname, 'data_out'))
 
-    def test_boyle(self):
+    def setUp(self):
+        make_and_clear_directory(self.output_dir)
+
+    def tearDown(self):
+        make_and_clear_directory(self.output_dir)
+
+    def create_test_output(self, infilename: str):
         mv = os.path.abspath(os.path.join(test_data_directory, 'fhir_metadata_vocabulary', 'fhir.ttl'))
         conf = os.path.abspath(os.path.join(test_conf_directory, 'db_conf'))
-        input_file = os.path.abspath(os.path.join(self.dirname, 'data', 'Boyle963_Kathleen891_83.ttl'))
-        output_dir = os.path.abspath(os.path.join(self.dirname, 'data_out'))
+        input_file = os.path.abspath(os.path.join(self.dirname, 'data', infilename))
+        load_facts("-mv {} --conf {} -i {} -t rdf -od {}".format(mv, conf, input_file, self.output_dir).split())
 
-        make_and_clear_directory(output_dir)
-        load_facts("-mv {} --conf {} -i {} -t rdf -od {}".format(mv, conf, input_file, output_dir).split())
-
-        with open(os.path.join(output_dir, 'observation_fact.tsv')) as output_tsv_file:
+    def test_boyle(self):
+        self.create_test_output('Boyle963_Kathleen891_83.ttl')
+        with open(os.path.join(self.output_dir, 'observation_fact.tsv')) as output_tsv_file:
             rslt = DictReader(output_tsv_file, delimiter="\t", quoting=QUOTE_NONE)
             sbp_idx = dbp_idx = sbp_res_idx = dbp_res_idx = 0
             for row in rslt:
@@ -61,10 +67,20 @@ class ObservationComponentTestCase(unittest.TestCase):
                     dbp_res_idx = row['instance_num']
                 elif row['nval_num'] == '172':
                     sbp_res_idx = row['instance_num']
-            make_and_clear_directory(output_dir)
-            self.assertTrue(sbp_idx == sbp_res_idx and dbp_idx == dbp_res_idx and sbp_idx != dbp_idx)
-        make_and_clear_directory(output_dir)
+        self.assertTrue(sbp_idx == sbp_res_idx and dbp_idx == dbp_res_idx and sbp_idx != dbp_idx)
 
+    def test_issue_6(self):
+        self.create_test_output('Boyle963_Kathleen891_83.ttl')
+        with open(os.path.join(self.output_dir, 'observation_fact.tsv')) as output_tsv_file:
+            rslt = DictReader(output_tsv_file, delimiter="\t", quoting=QUOTE_NONE)
+            inst_codes = {}            # type: Dict[int, str]
+            for row in rslt:
+                inst_num = int(row['instance_num'])
+                if inst_num > 0:
+                    if inst_num in inst_codes:
+                        self.assertEqual(inst_codes[inst_num], row['concept_cd'])
+                    else:
+                        inst_codes[inst_num] = row['concept_cd']
 
 if __name__ == '__main__':
     unittest.main()
