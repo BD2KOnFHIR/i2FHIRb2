1a80ed39d27773404800bec9dc8cc5dab4367109
hsolbrig <solbrig.harold@mayo.edu>
2018-02-06 09:14:57 -0600

    Add specific remove test facts script
    Temporary until we can get the actual issue fixed

--- tests/scripts/removetestfacts.py
+++ tests/scripts/removetestfacts.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017, Mayo Clinic
+# Copyright (c) 2018, Mayo Clinic
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
@@ -26,46 +26,16 @@
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import unittest
+# Remove any facts that have been left as the result of unit tests.
+#
+#  This should be run before and after any unit tests until we get the SQL issue resolved
 
 import os
+import sys
 
-from i2fhirb2.generate_i2b2 import generate_i2b2
-from tests.utils.script_test_base import ScriptTestBase
+if __name__ == "__main__":
+    sys.path.append(os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), '..'))
+    from i2fhirb2.removefacts import remove_facts
 
-
-class GenerateI2B2TestCase(ScriptTestBase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.dirname = os.path.split(os.path.abspath(__file__))[0]
-        cls.save_output = False
-        cls.tst_dir = "generatei2b2"
-        cls.tst_fcn = generate_i2b2
-        cls.conf_file_loc = "--conf {}".format(os.path.join(cls.dirname, 'data', 'db_conf'))
-
-    def test_no_args(self):
-        self.check_output_output("", "noargs")
-
-    def test_help(self):
-        self.check_output_output("-h", "help", exception=True)
-
-    def test_version(self):
-        self.check_output_output("-v", "version", exception=True)
-
-    def test_list(self):
-        self.check_output_output("--list " + self.conf_file_loc, "list")
-
-    def test_test(self):
-        self.save_output = True
-        self.check_output_output("--test " + self.conf_file_loc, "test", multipart_test=True)
-
-    @unittest.skipIf(False, "Connection should not be possible with bad user/password")
-    @unittest.expectedFailure
-    def test_bad_test(self):
-        bad_conf_file_loc = "--conf {}".format(os.path.join(self.dirname, 'data', 'db_conf_bad_password'))
-        self.check_output_output("--test " + bad_conf_file_loc, "badtest", exception=True)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    conf_file_loc = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'conf', 'db_conf'))
+    remove_facts(f"--conf {conf_file_loc} --removetestlist".split())
